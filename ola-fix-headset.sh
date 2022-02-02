8138b0701ade6051d44c9ac8883502f4cce499ea
Ola Chr. Våge <50892582+ovaag@users.noreply.github.com>
2022-02-02 15:50:32 +0100

    Update ola-fix-headset.sh

--- ola-fix-headset.sh
+++ ola-fix-headset.sh
@@ -5,21 +5,26 @@ set -e
 echo "Restarting bluetooth driver 🧢"
 systemctl restart bluetooth
 
-sleep 5
-
-echo "connect 30:50:75:48:76:CC" | bluetoothctl > /dev/null
-
-regex="([0-9]+)\sbluez"
-
-while [[ ! $(pactl list cards short) =~ $regex ]]
-do
-    echo "Waiting for headset to reconnect... 🔌"
-    sleep 2
-done
-
 sleep 2
 
+# Replace with desired mac adress
+mac_adress=30:50:75:48:76:CC
+
+#Connect to headset
+echo "connect ${mac_adress}" | bluetoothctl > /dev/null
+
+## Check if headset is in list of connected bluetooth devices
+# pactl uses underscore instead of colon in mac adress
+regex="([0-9]+)\s[a-z_]+.${mac_adress//:/_}"
+while [[ ! $(pactl list cards short) =~ $regex ]]; do
+    echo "Waiting for headset to reconnect... 🔌"
+    sleep 5
+done
+
+sleep 3
+
+## Set nice high-fidelity audio profile
 pactl_index=${BASH_REMATCH[1]}
-pacmd set-card-profile $pactl_index "a2dp_sink"
+pacmd set-card-profile "$pactl_index" "a2dp_sink"
 
 echo "Success 🎧🎧"
